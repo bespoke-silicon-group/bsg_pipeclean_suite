33699d24ab606256b20db82b787e530e4e27f357
Scott Davidson <scottdavidson1992@gmail.com>
2019-01-24 14:27:31 -0800

    Adding hierarchy to small_comb, medium_comb and large_comb designs.

--- small_comb/chip_top.v
+++ small_comb/chip_top.v
@@ -1,10 +1,24 @@
-module chip_top 
+module multiplier #(parameter width_p="inv")
+  ( input      [width_p-1:0] a_i
+  , input      [width_p-1:0] b_i
+  , output [(2*width_p)-1:0] c_o
+  );
+
+  assign c_o = a_i * b_i;
+
+endmodule
+
+module chip_top
   ( input   [7:0] a_i
   , input   [7:0] b_i
   , output [15:0] c_o
   );
 
-  assign c_o = a_i * b_i;
+  multiplier #(.width_p(8)) MULT 
+    (.a_i(a_i)
+    ,.b_i(b_i)
+    ,.c_o(c_o)
+    );
 
 endmodule
 
